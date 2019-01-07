3bf798e6a7d0ee14af89f89391c6c4f5cf4ce706
Tommy Chen <tommy351@gmail.com>
2019-01-08 01:02:27 +0800

    feat: Add kosko package as an alias of @kosko/cli

--- packages/kosko/bin/kosko.js
+++ packages/kosko/bin/kosko.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 const importLocal = require("import-local");
-const cli = require("../dist");
 
 if (!importLocal(__filename)) {
-  cli.run().catch(cli.handleError);
+  require("@kosko/cli/bin/kosko");
 }
