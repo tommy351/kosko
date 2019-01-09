18600bbb973f31e2bf9756e39c1dcbb33615e36b
Tommy Chen <tommy351@gmail.com>
2019-01-09 16:45:56 +0800

    test: Exclude index.ts from coverage report

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   preset: "ts-jest",
   roots: ["<rootDir>/packages", "<rootDir>/integration"],
-  collectCoverageFrom: ["packages/*/src/**/*.ts"],
+  collectCoverageFrom: ["packages/*/src/**/*.ts", "!packages/*/src/index.ts"],
   testEnvironment: "node",
   setupTestFrameworkScriptFile: "jest-extended",
   snapshotSerializers: ["jest-serializer-path"]
