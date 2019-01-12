32e896e896541ee08a4a1657cb6004909c991742
Tommy Chen <tommy351@gmail.com>
2019-01-13 02:19:53 +0800

    feat(config): Precompile validation script
    - Precompile validation script with "ajv-cli"
    - Move "schema.json" from "assets" folder to "dist" folder
    - Remove "ajv" from runtime dependencies of config package

--- packages/config/src/validate.ts
+++ packages/config/src/validate.ts
@@ -1,14 +1,17 @@
-import Ajv, { ErrorObject } from "ajv";
-
-const ajv = new Ajv();
+import { ErrorObject } from "ajv";
 
 // tslint:disable-next-line:no-var-requires
-const ajvValidate = ajv.compile(require("../assets/schema.json"));
+const ajvValidate = require("../dist/ajv-validate");
 
 export class ValidationError extends Error {
   constructor(public readonly errors: ErrorObject[]) {
-    super(ajv.errorsText(errors));
+    super(formatErrors(errors));
   }
+}
+
+function formatErrors(errors: ErrorObject[]): string {
+  if (!errors.length) return "No errors";
+  return errors.map(err => `${err.dataPath} ${err.message}`).join(", ");
 }
 
 export function validate(data: any) {
