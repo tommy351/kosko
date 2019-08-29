73e00804c13fc10dd50d5e0fdc4e358b3cf481a1
Tommy Chen <tommy351@gmail.com>
2019-08-29 23:43:39 +0800

    test(env): Test more data types for the merge func

--- packages/env/src/__tests__/merge.ts
+++ packages/env/src/__tests__/merge.ts
@@ -1,27 +1,59 @@
 import { merge } from "../merge";
 
-it("works", () => {
-  const actual = merge(
-    {
-      bar: 1,
-      foo: {
-        bar: 3
+describe("nested objects", () => {
+  it("should merge objects", () => {
+    const actual = merge(
+      {
+        bar: 1,
+        foo: {
+          bar: 3
+        }
+      },
+      {
+        baz: 2,
+        foo: {
+          baz: 4
+        }
       }
-    },
-    {
+    );
+
+    expect(actual).toEqual({
+      bar: 1,
       baz: 2,
       foo: {
+        bar: 3,
         baz: 4
       }
-    }
-  );
+    });
+  });
+});
 
-  expect(actual).toEqual({
-    bar: 1,
-    baz: 2,
-    foo: {
-      bar: 3,
-      baz: 4
+describe("array", () => {
+  it("should override the value", () => {
+    const actual = merge({ a: [1] }, { a: [2] });
+    expect(actual).toEqual({ a: [2] });
+  });
+});
+
+describe("non-plain objects", () => {
+  class Props {
+    constructor(props: any) {
+      for (const key of Object.keys(props)) {
+        (this as any)[key] = props[key];
+      }
     }
+  }
+
+  it("should override the value", () => {
+    const actual = merge(
+      {
+        a: new Props({ foo: 1, bar: 2 })
+      },
+      {
+        a: new Props({ foo: 3 })
+      }
+    );
+
+    expect(actual).toEqual({ a: new Props({ foo: 3 }) });
   });
 });
