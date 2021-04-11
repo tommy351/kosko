af6ec8b92106249a61c7362a1f6f671a49d66926
Tommy Chen <tommy351@gmail.com>
2021-04-12 01:41:15 +0800

    feat: Throw when importing unsupported functions

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,4 +1,15 @@
 export * from "./index.base";
 
 import { createAsyncEnvironment } from "./environment/async";
+import type { createNodeCJSEnvironment as _createNodeCJSEnvironment } from "./environment/node-cjs";
+import type { createNodeESMEnvironment as _createNodeESMEnvironment } from "./environment/node-esm";
+
 export default createAsyncEnvironment();
+
+export const createNodeCJSEnvironment: typeof _createNodeCJSEnvironment = () => {
+  throw new Error("createNodeCJSEnvironment is only supported on Node.js");
+};
+
+export const createNodeESMEnvironment: typeof _createNodeESMEnvironment = () => {
+  throw new Error("createNodeESMEnvironment is only supported on Node.js");
+};
--- packages/generate/src/index.ts
+++ packages/generate/src/index.ts
@@ -1,1 +1,7 @@
+import type { generate as _generate } from "./generate";
+
 export * from "./index.base";
+
+export const generate: typeof _generate = () => {
+  throw new Error("generate is only supported on Node.js");
+};
