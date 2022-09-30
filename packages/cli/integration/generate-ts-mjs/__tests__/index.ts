38370d372c898c2ee5dd7c3b0b0396de8ae7d71e
Tommy Chen <tommy351@gmail.com>
2022-10-01 01:00:02 +0800

    test(integration): Update CLI generate loader tests

--- packages/cli/integration/generate-ts-mjs/__tests__/index.ts
+++ packages/cli/integration/generate-ts-mjs/__tests__/index.ts
@@ -1,20 +1,22 @@
-import execa from "execa";
 import { dirname } from "path";
 import { installPackage, runCLI } from "@kosko/integration-test-utils";
 
 const testDir = dirname(__dirname);
-let result: execa.ExecaReturnValue;
 
 beforeAll(async () => {
   await installPackage(testDir, "env");
 });
 
-beforeEach(async () => {
-  result = await runCLI(["generate"], {
+test("should load loaders specified in args", async () => {
+  const result = await runCLI(["generate", "--loader", "ts-node/esm"], {
     cwd: testDir
   });
+  expect(result.stdout).toMatchSnapshot();
 });
 
-test("should import ESM files", () => {
+test("should load loaders specified in config", async () => {
+  const result = await runCLI(["generate", "--config", "kosko-loader.toml"], {
+    cwd: testDir
+  });
   expect(result.stdout).toMatchSnapshot();
 });
