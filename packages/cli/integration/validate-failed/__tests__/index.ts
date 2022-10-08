19fab0a41a4c1e5c3348bb3fff6259098b67480f
Tommy Chen <tommy351@gmail.com>
2022-10-08 11:57:03 +0800

    chore(pnpm): Set `save-workspace-protocol = rolling`

--- packages/cli/integration/validate-failed/__tests__/index.ts
+++ packages/cli/integration/validate-failed/__tests__/index.ts
@@ -19,8 +19,5 @@ test("should return status code 1", () => {
 });
 
 test("should print the error", () => {
-  // expect(result.stderr).toContain(
-  //   "Error: data/spec must have required property 'containers', data/spec must be null, data/spec must match exactly one schema in oneOf"
-  // );
   expect(result.stderr).toMatchSnapshot();
 });
