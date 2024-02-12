dea18f05f44f9909cde71733555477a9d674e257
Tommy Chen <tommy351@gmail.com>
2024-02-12 23:13:29 +0800

    fix: Fix inconsistent snapshot files
    Disable console colors in executable files in integration tests.

--- packages/yaml/integration/load-url/index.mjs
+++ packages/yaml/integration/load-url/index.mjs
@@ -1,6 +1,13 @@
 import { env } from "node:process";
 import { loadUrl } from "@kosko/yaml";
+import { Console } from "node:console";
+
+const logger = new Console({
+  stdout: process.stdout,
+  stderr: process.stderr,
+  colorMode: false
+});
 
 const result = await loadUrl(`http://${env.SERVER_ADDRESS}`)();
 
-console.log(result);
+logger.log(result);
