cfa14507dec209065ad908cc93a7f304c1c3198e
Tommy Chen <tommy351@gmail.com>
2024-03-20 19:22:59 +0800

    test(common-utils): Add `getManifestMeta` tests

--- packages/common-utils/src/__tests__/index.ts
+++ packages/common-utils/src/__tests__/index.ts
@@ -1,4 +1,10 @@
-import { apiVersionToGroup, getErrorCode, isRecord, toArray } from "../index";
+import {
+  apiVersionToGroup,
+  getErrorCode,
+  getManifestMeta,
+  isRecord,
+  toArray
+} from "../index";
 
 class Empty {}
 
@@ -54,3 +60,64 @@ test.each([
     expect(apiVersionToGroup(apiVersion)).toEqual(expected);
   }
 );
+
+describe("getManifestMeta", () => {
+  test("returns undefined when value is undefined", () => {
+    expect(getManifestMeta(undefined)).toBeUndefined();
+  });
+
+  test("returns undefined when value is an empty object", () => {
+    expect(getManifestMeta({})).toBeUndefined();
+  });
+
+  test("returns undefined when apiVersion is undefined", () => {
+    expect(
+      getManifestMeta({ kind: "Pod", metadata: { name: "test" } })
+    ).toBeUndefined();
+  });
+
+  test("returns undefined when kind is undefined", () => {
+    expect(
+      getManifestMeta({ apiVersion: "v1", metadata: { name: "test" } })
+    ).toBeUndefined();
+  });
+
+  test("returns undefined when metadata is undefined", () => {
+    expect(getManifestMeta({ apiVersion: "v1", kind: "Pod" })).toBeUndefined();
+  });
+
+  test("returns undefined when name is undefined", () => {
+    expect(
+      getManifestMeta({ apiVersion: "v1", kind: "Pod", metadata: {} })
+    ).toBeUndefined();
+  });
+
+  test("returns metadata when all fields are defined", () => {
+    expect(
+      getManifestMeta({
+        apiVersion: "v1",
+        kind: "Pod",
+        metadata: { name: "test" }
+      })
+    ).toEqual({
+      apiVersion: "v1",
+      kind: "Pod",
+      name: "test"
+    });
+  });
+
+  test("returns namespace when it is defined", () => {
+    expect(
+      getManifestMeta({
+        apiVersion: "v1",
+        kind: "Pod",
+        metadata: { name: "test", namespace: "default" }
+      })
+    ).toEqual({
+      apiVersion: "v1",
+      kind: "Pod",
+      name: "test",
+      namespace: "default"
+    });
+  });
+});
