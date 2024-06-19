05b2a456f7b21316acff2793739cdb714f024f05
Tommy Chen <tommy351@gmail.com>
2024-06-19 19:58:55 +0800

    tmp [ci skip]

--- packages/cli/integration/generate-ts-mjs/components/nginx.ts
+++ packages/cli/integration/generate-ts-mjs/components/nginx.ts
@@ -1,7 +1,11 @@
 import { Pod } from "kubernetes-models/v1/Pod";
+import env from "@kosko/env";
+
+const params = await env.global();
 
 export default new Pod({
   metadata: {
-    name: "test-pod"
+    name: "test-pod",
+    annotations: params
   }
 });
